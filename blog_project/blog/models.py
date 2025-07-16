0596544f97c798fdd47acc6ec1c78c1c8d15ea3e
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-16 19:33:33 +0330

    Add author field to Post model as foreign key

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -10,6 +11,7 @@ class Post(models.Model):
     slug = models.CharField(max_length=250)
     body = models.TextField()
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
