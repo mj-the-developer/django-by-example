489a245194a473dca9176d9f7400dee81fee543b
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-18 20:56:59 +0330

    Add post feeds

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from blog import views
+from blog.feeds import LatestPostsFeed
 
 
 app_name = 'blog'
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -12,4 +14,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
