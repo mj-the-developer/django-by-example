92b970fe9b10e38816ec5bdbd8831175d4f2ff7a
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-18 10:00:46 +0330

    Add comment model to admin site

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from blog.models import Post
+from blog.models import Comment, Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -12,3 +12,10 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
     show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']
+    raw_id_fields = ['post']
