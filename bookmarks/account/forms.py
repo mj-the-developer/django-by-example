4c7390982165a25649b539a93a915fc49ed97eba
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-19 10:54:46 +0330

    Prevent registration or edit profiles with existing email addresses

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 from account.models import Profile
 
@@ -14,7 +14,7 @@ class UserRegistrationForm(forms.ModelForm):
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['username', 'first_name', 'email']
 
     def clean_password2(self):
@@ -22,12 +22,25 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Passwords do not match.")
         return cd['password2']
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email already in use.')
+        return email
 
 
 class UserEditForm(forms.ModelForm):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=email)
+        if qs.exists():
+            raise forms.ValidationError('Email already in use.')
+        return email
 
 
 class ProfileEditForm(forms.ModelForm):
