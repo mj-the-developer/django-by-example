0883f978789a2d5e8de4cc3cfc5f17025b833bfd
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-19 23:37:01 +0330

    Add create profile pipeline after user create pipeline in social auth pipelines

--- bookmarks/account/authentication.py
+++ bookmarks/account/authentication.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 
+from account.models import Profile
+
 
 class EmailAuthBackend:
     def authenticate(self, request, username=None, password=None):
@@ -16,3 +18,6 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+        
+    def create_profile(backend, user, *args, **kwargs):
+        Profile.objects.get_or_create(user=user)
