eb582691deda6491ead279ed1db06c88a9af3e27
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-19 09:30:15 +0330

    Add static urls for development

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -132,3 +132,8 @@ LOGOUT_URL = 'logout'
 # Email configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Media configuration
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
