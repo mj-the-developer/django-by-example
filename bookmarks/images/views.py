02ad75b80d4dde141551b36f64360fca0c4334a8
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-20 22:18:03 +0330

    Add API view and javascript to like/unlike images'

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,7 +1,8 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 
 from images.forms import ImageCreateForm
 from images.models import Image
@@ -26,3 +27,23 @@ def image_create(request: HttpRequest):
 def image_detail(request: HttpRequest, id: int, slug: str):
     image = get_object_or_404(Image, id=id, slug=slug)
     return render(request, 'images/image/detail.html', {'secion': 'images', 'image': image})
+
+
+@login_required
+@require_POST
+def image_like(request: HttpRequest):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+
+    return JsonResponse({'status': 'error'})
