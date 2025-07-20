530b92fca1064ee96a9d02f36a5bab5370c34fb5
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-20 18:57:34 +0330

    Add create image form

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -13,16 +13,20 @@ class Image(models.Model):
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
+
     class Meta:
         ordering = ['-created']
         indexes = [
             models.Index(fields=['-created']),
         ]
 
+
     def __str__(self):
         return self.title
     
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
+
         super().save(*args, **kwargs)
