4a57f65802e65a403934852dac701640c1aa9c8c
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-20 18:02:41 +0330

    Add many to many relation ships between images and users who liked them

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -4,12 +4,13 @@ from django.utils.text import slugify
 
 
 class Image(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='images_created')
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.TextField(blank=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
