a0714bbd56acd4870618d4d6ad22f176e6707d84
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-21 20:55:16 +0330

    Add target field as content type to action model to take advantage of generic relationships

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -6,10 +8,14 @@ class Action(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='actions', on_delete=models.CASCADE)
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
+    target_ct = models.ForeignKey(ContentType, blank=True, null=True, related_name='target_obj', on_delete=models.CASCADE)
+    target_id = models.PositiveBigIntegerField(null=True, blank=True)
+    target = GenericForeignKey('target_ct', 'target_id')
 
 
     class Meta:
         ordering = ['-created']
         indexes = [
             models.Index(fields=['-created']),
+            models.Index(fields=['target_ct', 'target_id'])
         ]
