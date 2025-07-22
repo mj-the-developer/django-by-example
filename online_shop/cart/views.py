54e31aaf57f151161f0cb36ef258abe05d215765
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-22 19:13:26 +0330

    Add cart views, urls and templates

--- online_shop/cart/views.py
+++ online_shop/cart/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from shop.models import Product
+from cart.cart import Cart
+from cart.forms import CartAddProductForm
+
+
+@require_POST
+def cart_add(request: HttpRequest, product_id: int):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product, quantity=cd['quantity'], override_quantity=cd['override'])
+
+    return redirect('cart:cart_detail')
+
+
+@require_POST
+def cart_remove(request: HttpRequest, product_id: int):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request: HttpRequest):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
