b97bc9f32ab208ca873312f25ead0479806f925c
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-22 20:43:25 +0330

    Add order views, urls and templates

--- online_shop/orders/forms.py
+++ online_shop/orders/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from orders.models import Order
 
 
-class OrderForm(forms.ModelForm):
-    model = Order
-    fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
--- online_shop/orders/views.py
+++ online_shop/orders/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from cart.cart import Cart
+from orders.models import OrderItem
+from orders.forms import OrderCreateForm
+
+
+def order_create(request: HttpRequest):
+    cart = Cart(request)
+    
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order ,product=item['product'], price=item['price'], quantity=item['quantity'])
+            cart.clear()
+            return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+
+    return render(request, 'orders/order/create.html', {'form': form, 'cart': cart})
