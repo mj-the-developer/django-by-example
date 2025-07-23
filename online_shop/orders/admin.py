3887ffe74498bf3601a4cfdb6a288acbf2d73aec
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-24 01:16:05 +0330

    Add method columns to django admin site

--- online_shop/orders/admin.py
+++ online_shop/orders/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from orders.models import Order, OrderItem
 
@@ -8,8 +9,28 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def stripe_payment_url(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
+    list_display = [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'address',
+        'postal_code',
+        'city',
+        'paid',
+        stripe_payment_url,
+        'created',
+        'updated'
+    ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
