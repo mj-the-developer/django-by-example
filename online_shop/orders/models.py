087325ce7bc34f2ea9a2fdaa5a2e644e7556c48d
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-24 01:07:12 +0330

    Add get_stripe_url method to order model

--- online_shop/orders/models.py
+++ online_shop/orders/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from shop.models import Product
@@ -29,6 +30,18 @@ class Order(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+    
+
+    def get_stripe_url(self):
+        if not self.stripe_id:
+            return
+        
+        if '_test_' in settings.STRIPE_SECRET_KEY:
+            path = '/test/'
+        else:
+            path = '/'
+
+        return f"https://dashboard.stripe.com{path}payments/{self.stripe_id}"
 
 
 class OrderItem(models.Model):
