f98b84fd9628fc8748c4e347fa98eb7535ac7c13
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-23 21:02:33 +0330

    Add payment app

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
+    'payment.apps.PaymentConfig',
     'shop.apps.ShopConfig',
 ]
 
