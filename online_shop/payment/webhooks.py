27d32ddfdd5c638ba7e14845acf2fd88f6e54a0a
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-25 01:19:23 +0330

    Add recommended logic to product detail view

--- online_shop/payment/webhooks.py
+++ online_shop/payment/webhooks.py
@@ -4,6 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 import stripe
 
 from orders.models import Order
+from shop.models import Product
+from shop.recommender import Recommender
 from payment.tasks import payment_completed
 
 
@@ -31,6 +33,11 @@ def stripe_webhook(request: HttpRequest):
             order.paid = True
             order.stripe_id = session.payment_intent
             order.save()
+
+            product_ids = order.items.values_list('product_id')
+            products = Product.objects.filter(id__in=product_ids)
+            r = Recommender()
+            r.products_bought(products)
     
     payment_completed.delay(order.id)
     return HttpResponse(status=200)
--- online_shop/shop/views.py
+++ online_shop/shop/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
 
 from cart.forms import CartAddProductForm
+from shop.recommender import Recommender
 from shop.models import Category, Product
 
 
@@ -20,4 +21,12 @@ def product_list(request: HttpRequest, category_slug=None):
 def product_detail(request: HttpRequest, id: int, slug: str):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
-    return render(request, 'shop/product/detail.html', {'product': product, 'cart_product_form': cart_product_form})
+
+    r = Recommender()
+    recommended_products = r.suggest_products_for([product], 4)
+
+    return render(request, 'shop/product/detail.html', {
+        'product': product,
+        'cart_product_form': cart_product_form,
+        'recommended_products': recommended_products,
+    })
