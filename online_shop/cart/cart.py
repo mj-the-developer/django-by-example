f693803c4580cf75c10e2f118afc8a1f9c54cc81
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-24 23:59:11 +0330

    Add coupon logic to card class

--- online_shop/cart/cart.py
+++ online_shop/cart/cart.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from django.conf import settings
 from django.http import HttpRequest
 
+from coupons.models import Coupon
 from shop.models import Product
 
 
@@ -15,6 +16,7 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
 
         self.cart = cart
+        self.coupon_id = self.session.get('coupon_id')
 
 
     def add(self, product: Product, quantity=1, override_quantity=False):
@@ -71,3 +73,24 @@ class Cart:
     
     def save(self):
         self.session.modified = True
+
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+
+        return None
+    
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        return Decimal(0)
+    
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
