33acb14b540671adbbe1532af62e2a544b2680b6
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-25 20:32:29 +0330

    Add content create update view, url and template

--- education/courses/views.py
+++ education/courses/views.py
@@ -1,3 +1,6 @@
+from django.apps import apps
+from django.db.models import Model
+from django.forms.models import modelform_factory
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, redirect
 from django.views.generic.base import TemplateResponseMixin, View
@@ -6,7 +9,7 @@ from django.views.generic.list import ListView
 
 from courses.forms import ModuleFormSet
 from courses.mixins import OwnerCourseEditMixin, OwnerCourseMixin
-from courses.models import Course
+from courses.models import Content, Course, Module
 
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
@@ -50,3 +53,47 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
             return redirect('course:manage_course_list')
         
         return self.render_to_response({'course': self.course, 'formset': formset})
+
+
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+
+    def get_model(self, model_name: str):
+        if model_name in ['text', 'video', 'image', 'file']:
+            return apps.get_model(app_label='courses', model_name=model_name)
+        return None
+    
+    def get_form(self, model: Model, *args, **kwargs):
+        Form = modelform_factory(model, exclude=['owner', 'order', 'created', 'updated'])
+        return Form(*args, **kwargs)
+    
+    def dispatch(self, request: HttpRequest, module_id: int, model_name: str, id=None):
+        self.module = get_object_or_404(Module, id=module_id, course__owner=request.user)
+        self.model = self.get_model(model_name)
+
+        if id:
+            self.obj = get_object_or_404(self.model, id=id, owner=request.user)
+
+        return super().dispatch(request, module_id, model_name, id)
+    
+    def get(self, request: HttpRequest, module_id: int, model_name: str, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form, 'object': self.obj})
+    
+    def post(self, request: HttpRequest, module_id: int, model_name: str, id=None):
+        form = self.get_form(self.model, instance=self.obj, data=request.POST, files=request.FILES)
+
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            
+            if not id:
+                Content.objects.create(module=self.module, item=obj)
+
+            return redirect('course:module_content_list', self.module.id)
+        
+        return self.render_to_response({'form': form, 'object': self.obj})
