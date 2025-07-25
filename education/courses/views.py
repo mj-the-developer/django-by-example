64863aaadc278c8a258401ad267b3f253f13b62f
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-25 17:49:27 +0330

    Add urls and templates for course views

--- education/courses/views.py
+++ education/courses/views.py
@@ -21,7 +21,7 @@ class OwnerEditMixin:
 class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
     model = Course
     fields = ['subject', 'title', 'slug', 'overview']
-    success_url = reverse_lazy('manage_course_list')
+    success_url = reverse_lazy('course:manage_course_list')
 
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
