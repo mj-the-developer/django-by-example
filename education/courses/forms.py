6d5264d2134acb958a804ba7d33f0d8f68762852
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-25 18:09:39 +0330

    Add module formset

---
+++ education/courses/forms.py
@@ -0,0 +1,6 @@
+from django.forms.models import inlineformset_factory
+
+from courses.models import Course, Module
+
+
+ModuleFormSet = inlineformset_factory(Course, Module, fields=['title', 'description'], extra=2, can_delete=True)
