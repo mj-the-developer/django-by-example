50248dc24b4af7c95671bdfdd07b2aa72a4ec815
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-26 00:39:16 +0330

    Add enroll course view

--- education/courses/views.py
+++ education/courses/views.py
@@ -13,6 +13,7 @@ from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from courses.forms import ModuleFormSet
 from courses.mixins import OwnerCourseEditMixin, OwnerCourseMixin
 from courses.models import Content, Course, Module, Subject
+from students.forms import CourseEnrollForm
 
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
@@ -151,3 +152,8 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course': self.object})
+        return context
