b16b805914698f9949172f905d8963dc30032f18
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-25 10:05:43 +0330

    Add language locale to URLs

--- online_shop/online_shop/urls.py
+++ online_shop/online_shop/urls.py
@@ -1,7 +1,8 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
@@ -9,4 +10,4 @@ urlpatterns = [
     path('coupons/', include('coupons.urls', namespace='coupons')),
     path('rosetta/', include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
-]
+)
