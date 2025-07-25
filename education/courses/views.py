ff7f456770978eedb198b1148c13d54ceb8e5585
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-25 17:56:43 +0330

    Refactor views and move mixins to their own file

--- education/courses/views.py
+++ education/courses/views.py
@@ -1,31 +1,7 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
-from courses.models import Course
-
-
-class OwnerMixin:
-    def get_queryset(self):
-        qs = super().get_queryset()
-        return qs.filter(owner=self.request.user)
-
-
-class OwnerEditMixin:
-    def form_valid(self, form):
-        form.instance.owner = self.request.user
-        return super().form_valid(form)
-    
-
-class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
-    model = Course
-    fields = ['subject', 'title', 'slug', 'overview']
-    success_url = reverse_lazy('course:manage_course_list')
-
-
-class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
-    template_name = 'courses/manage/course/form.html'
+from courses.mixins import OwnerCourseEditMixin, OwnerCourseMixin
 
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
