62bf79fdf0d23a13dc0c55d812737b6f46a28591
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-25 22:49:46 +0330

    Use HTML5 sortable to change orders of modules and contents with drag & drop

--- education/courses/models.py
+++ education/courses/models.py
@@ -46,7 +46,7 @@ class Module(models.Model):
 
 
 class Content(models.Model):
-    module = models.ForeignKey(Module, related_name='modules', on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
     content_type = models.ForeignKey(
         ContentType,
         on_delete=models.CASCADE,
