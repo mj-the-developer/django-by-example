453539d6073ef3e79ebf781265c74eb48b3a17af
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-27 18:59:02 +0330

    Add popular courses to serializer as serializer method field

--- education/courses/api/serializers.py
+++ education/courses/api/serializers.py
@@ -1,10 +1,17 @@
+from django.db.models import Count
 from rest_framework import serializers
+
 from courses.models import Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     total_courses = serializers.IntegerField()
+    popular_courses = serializers.SerializerMethodField()
+
+    def get_popular_courses(self, obj: Subject):
+        courses = obj.courses.annotate(total_students=Count('students')).order_by('-total_students')[:3]
+        return [f"{c.title} ({c.total_students})" for c in courses]
 
     class Meta:
         model = Subject
-        fields = ['id', 'title', 'slug', 'total_courses']
+        fields = ['id', 'title', 'slug', 'total_courses', 'popular_courses']
