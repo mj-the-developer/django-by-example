228a35552d82ae3f0a3debbd1803bc2ee1494a4c
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-27 19:21:49 +0330

    Use nested serializers to serialize relation ship with selected attributes on their serializer

--- education/courses/api/serializers.py
+++ education/courses/api/serializers.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from rest_framework import serializers
 
-from courses.models import Course, Subject
+from courses.models import Course, Module, Subject
 
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -17,8 +17,14 @@ class SubjectSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'slug', 'total_courses', 'popular_courses']
 
 
+class ModuleSerializer:
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
 class CourseSerializer(serializers.ModelSerializer):
-    modules = serializers.StringRelatedField(many=True, read_only=True)
+    modules = ModuleSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
