c2ce403f6fe283ea3bb5015b5a04c16e2a798adb
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2025-07-28 09:33:37 +0330

    Persist chat messages in database

--- education/chat/consumers.py
+++ education/chat/consumers.py
@@ -3,6 +3,8 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.utils import timezone
 
+from chat.models import Message
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -27,6 +29,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'user': self.user.username,
             'datetime': now.isoformat(),
         })
+        await self.persist_message(message)
 
     async def chat_message(self, event):
         await self.send(text_data=json.dumps(event))
+
+    async def persist_message(self, message):
+        await Message.objects.acreate(user=self.user, course_id=self.id, content=message)
